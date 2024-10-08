5582873cd0df53d2e2fc8d6ddcd219d02b51ca6d
machida-yosuke <machida-yosuke@kayac.com>
2024-10-08 11:31:08 +0900

    モジュール

--- app/main.go
+++ app/main.go
@@ -1,78 +1,110 @@
 package main
 
 import (
+	"app/mysql"
+	"app/typefile"
+	"errors"
 	"fmt"
-	"time"
 
+	"github.com/bwmarrin/snowflake"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
-type User struct {
-	gorm.Model
-	Name       string
-	Department string
-	Email      string
-	Phone      string
-}
-
 func main() {
-	db := sqlConnect()
-	db.AutoMigrate(&User{})
+	db := mysql.SqlConnect()
+	// db.AutoMigrate().DropTable(&Region{}, &User{}, &Team{}, &GameEntry{}, &GameScore{}, &Game{})
+	db.AutoMigrate(&typefile.Region{}, &typefile.User{}, &typefile.Team{}, &typefile.GameEntry{}, &typefile.GameScore{}, &typefile.Game{})
+	mysql.Seed(db)
 	defer db.Close()
 
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World",
+			"message": "Hello",
 		})
 	})
 
-	r.GET("/users", func(c *gin.Context) {
-		var users []User
-		db.Find(&users)
-		c.JSON(200, users)
+	r.POST("/teams", func(c *gin.Context) {
+
+		node, err := snowflake.NewNode(1)
+		if err != nil {
+			panic(err)
+		}
+
+		var json typefile.JsonRequest
+
+		// リクエストボディをJSONとしてパースして構造体にマッピング
+		if err := c.BindJSON(&json); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		// 受け取ったデータをログに出力
+		fmt.Printf("RegionId: %d, Users: %v\n", json.RegionId, json.Users)
+
+		var region typefile.Region
+		if err := db.First(&region, json.RegionId).Error; err != nil {
+			c.JSON(400, gin.H{"error": "Region not found"})
+			return
+		}
+
+		uuid := node.Generate()
+		fmt.Println("Generated Snowflake ID:", uuid)
+
+		// チームを作成
+		team := typefile.Team{Name: "チーム1", Region: region, Uuid: uuid.String()}
+		db.Create(&team)
+
+		// ユーザーを作成
+		for _, userName := range json.Users {
+			db.Create(&typefile.User{Name: userName, Team: team})
+		}
+
+		c.JSON(200, team)
 	})
 
-	r.GET("/users/:id", func(c *gin.Context) {
-		var user User
-		db.First(&user, c.Param("id"))
-		c.JSON(200, user)
+	r.GET("/teams/:uuid", func(c *gin.Context) {
+		var team typefile.Team
+		result := db.Preload("User, Region").Where("uuid = ?", c.Param("uuid")).First(&team)
+		fmt.Println(result.Value)
+
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(404, gin.H{"code": "1000", "message": "team Not Found"})
+			return
+		}
+
+		c.JSON(200, result)
+	})
+
+	r.GET("/games", func(c *gin.Context) {
+		var games []typefile.Game
+		// 全て取得
+		result := db.Find(&games)
+		fmt.Println(result.Value)
+
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(404, gin.H{"code": "1000", "message": "games Not Found"})
+			return
+		}
+
+		c.JSON(200, result)
+	})
+
+	r.GET("/regions", func(c *gin.Context) {
+		var regions []typefile.Region
+		result := db.Find(&regions)
+		fmt.Println(result.Value)
+
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(404, gin.H{"code": "1000", "message": "regions Not Found"})
+			return
+		}
+
+		c.JSON(200, result)
 	})
 
 	r.Run(":8080")
-}
-
-func sqlConnect() (database *gorm.DB) {
-	DBMS := "mysql"
-	USER := "go_test"
-	PASS := "password"
-	PROTOCOL := "tcp(db:3306)"
-	DBNAME := "go_database"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-
-	count := 0
-	db, err := gorm.Open(DBMS, CONNECT)
-	if err != nil {
-		for {
-			if err == nil {
-				fmt.Println("")
-				break
-			}
-			fmt.Print(".")
-			time.Sleep(time.Second)
-			count++
-			if count > 180 {
-				fmt.Println("")
-				fmt.Println("DB接続失敗")
-				panic(err)
-			}
-			db, err = gorm.Open(DBMS, CONNECT)
-		}
-	}
-	fmt.Println("DB接続成功")
-
-	return db
 }
