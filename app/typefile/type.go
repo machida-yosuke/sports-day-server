a698af6a5173b0f562bb1e8e3552eb399d3f4539
machida-yosuke <machida-yosuke@kayac.com>
2024-10-09 16:59:09 +0900

    response

--- app/typefile/type.go
+++ app/typefile/type.go
@@ -27,17 +27,19 @@ type Team struct {
 
 type GameEntry struct {
 	gorm.Model
-	TeamID      uint
-	GameID      uint
-	Game        Game
-	Team        Team
+	TeamID      uint `gorm:"index"`
+	Team        Team `gorm:"foreignKey:TeamID"`
+	GameID      uint `gorm:"index"`
+	Game        Game `gorm:"foreignKey:GameID"`
 	GameScoreID uint
-	GameScore   GameScore
+	GameScore   GameScore `gorm:"foreignKey:GameScoreID"`
 }
 
 type GameScore struct {
 	gorm.Model
-	Score uint
+	Score       uint
+	HelpScore   uint
+	HelperCount uint
 }
 
 type Game struct {
@@ -45,7 +47,39 @@ type Game struct {
 	Name string
 }
 
-type JsonRequest struct {
+type TeamCreateJsonRequest struct {
 	RegionId int      `json:"region_id"`
 	Users    []string `json:"users"`
+}
+
+type ResultCreateJsonRequest struct {
+	Entries []GameEntryJson `json:"entries"`
+}
+
+type GameEntryJson struct {
+	GameId      uint `json:"game_id"`
+	Score       uint `json:"score"`
+	HelpScore   uint `json:"help_score"`
+	HelperCount uint `json:"helper_count"`
+}
+
+type ResultJsonResponse struct {
+	TeamID     uint   `json:"team_id"`
+	TeamUuid   string `json:"team_uuid"`
+	RegionName string `json:"region_name"`
+
+	UserNames  []string   `json:"user_names"`
+	TotalScore uint       `json:"total_score"`
+	TeamRank   uint       `json:"team_rank"`
+	GameRanks  []GameRank `json:"game_ranks"`
+
+	RegionTotalUserCount            uint `json:"region_total_user_count"`
+	Top10TeamsScoreRankingByRegion  uint `json:"top10_teams_score_ranking_by_region"`
+	EnteredRegionCountInCompetition uint `json:"entered_region_count_in_competition"`
+}
+
+type GameRank struct {
+	GameID uint `json:"game_id"`
+	Score  uint `json:"score"`
+	Rank   uint `json:"rank"`
 }
